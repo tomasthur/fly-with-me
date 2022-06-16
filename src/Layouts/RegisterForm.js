f3b180fc16dc28aed1d08ef612882918a2e10c1d
Tomas Thur <thur.tomas@gmail.com>
2022-06-16 10:48:39 +0200

    make useFormik RegisterForm done

--- src/Layouts/RegisterForm.js
+++ src/Layouts/RegisterForm.js
@@ -1,14 +1,74 @@
 import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import RegisterFormHeadline from "../Components/RegisterFormHeadline";
+import PilotImage from "../assets/3.png";
 
 const RegisterForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      firstname: "",
+      lastname: "",
+      companyname: "",
+      nationality: "",
+    },
+    validationSchema: Yup.object({
+      firstname: Yup.string().required("required"),
+      lastname: Yup.string().required("required"),
+      companyname: Yup.string().required("required"),
+    }),
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   return (
-    <div className="register-form">
+    <form onSubmit={formik.handleSubmit} className="register-form">
+      <img src={PilotImage} id="pilot-image" alt="" />
       <RegisterFormHeadline />
-      <input type="text" placeholder="First name" />
-      <input type="text" placeholder="Last name" />
-      <input type="text" placeholder="Company name"/>
-      <select name="nationality" id="nationality">
+      <input
+        type="text"
+        id="firstname"
+        name="firstname"
+        placeholder="First name"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.firstname}
+      />
+      {formik.touched.firstname && formik.errors.firstname ? (
+        <div>{formik.errors.firstname}</div>
+      ) : null}
+      <input
+        type="text"
+        id="lastname"
+        name="lastname"
+        placeholder="Last name"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.lastname}
+      />
+      {formik.touched.lastname && formik.errors.lastname ? (
+        <div>{formik.errors.lastname}</div>
+      ) : null}
+      <input
+        type="text"
+        id="comapnyname"
+        name="companyname"
+        placeholder="Company name"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.companyname}
+      />
+      {formik.touched.companyname && formik.errors.companyname ? (
+        <div>{formik.errors.companyname}</div>
+      ) : null}
+      <select
+        name="nationality"
+        id="nationality"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.nationality}
+      >
         <option value="Slovakia">Slovakia</option>
         <option value="Czech">Czech</option>
         <option value="Poland">Poland</option>
@@ -17,7 +77,7 @@ const RegisterForm = () => {
         <option value="Italy">Italy</option>
       </select>
       <input id="submit-button" type="submit" value="Register your company" />
-    </div>
+    </form>
   );
 };
 
