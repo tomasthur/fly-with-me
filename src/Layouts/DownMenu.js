8c8864dfbbc3938b0b3913f215e8c61e5a611473
Tomas Thur <thur.tomas@gmail.com>
2022-07-06 15:30:59 +0200

    add DownMenu.js and some css changes

--- src/Layouts/DownMenu.js
+++ src/Layouts/DownMenu.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const DownMenu = () => {
   return (
-    <div className='down-menu'>
-            <div className="row">
-              <button>Hire pilots</button>
-            </div>
+    <div className="down-menu">
+      <div className="row">
+        <button>Pilots Offer</button>
+        <button>Airplanes Offer</button>
+        <button>Airports Offer</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default DownMenu
+export default DownMenu;
